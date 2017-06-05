a941c5ac4f0a37068b93c1b4d28b7829c2dbd5bf
yeyongping <yeyongping@qq.com>
2017-06-05 15:14:27 +0800

    update YPSlider param setting

--- YPKit/YPKitFramework/YPSlider.h
+++ YPKit/YPKitFramework/YPSlider.h
@@ -10,5 +10,11 @@
 
 @interface YPSlider : UIScrollView
 
+@property (copy, nonatomic) NSString *title;
+@property (strong, nonatomic) UIColor *titleColor;
+@property (strong, nonatomic) UIFont *titleFont;
+
 + (instancetype)sliderWithFrame:(CGRect)frame sliderToRight:(void(^)(BOOL isRight))sliderHandle;
+- (void)sliderToRight:(void(^)(BOOL isRight))sliderHandle;
+
 @end
--- YPKit/YPKitFramework/YPSlider.m
+++ YPKit/YPKitFramework/YPSlider.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) UIView *slider;
 @property (weak, nonatomic) UIView *leftBackgroundView;
 @property (copy, nonatomic) void(^sliderHandle)( BOOL);
+@property (weak, nonatomic) UILabel *titleLabel;
 
 @end
 
@@ -28,27 +29,41 @@
 
 
 + (instancetype)sliderWithFrame:(CGRect)frame sliderToRight:(void(^)(BOOL))sliderHandle{
-    CGFloat height = CGRectGetHeight(frame);
-    CGFloat width = CGRectGetWidth(frame);
-    YPSlider *sliderView = [[YPSlider alloc] initWithFrame:frame];
-    
+    YPSlider *sliderView = [[self alloc] initWithFrame:frame];
     sliderView.sliderHandle = sliderHandle;
-    sliderView.layer.cornerRadius = height/2;
-    sliderView.layer.masksToBounds = YES;
-    sliderView.bounces = NO;
-    sliderView.showsHorizontalScrollIndicator = NO;
-    sliderView.pagingEnabled = YES;
-    sliderView.backgroundColor = [UIColor greenColor];
-    sliderView.contentSize = CGSizeMake(2*width - height, 0);
-    sliderView.contentOffset = CGPointMake(width - height, 0);
-    
-    [sliderView setupSubviews];
     return sliderView;
 }
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self setupSubviews];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setupSubviews];
+    }
+    return self;
+}
+
 - (void)setupSubviews {
+    
+    [self addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
     CGFloat width = CGRectGetWidth(self.frame);
     CGFloat height = CGRectGetHeight(self.frame);
+    
+    self.bounces = NO;
+    self.showsHorizontalScrollIndicator = NO;
+    self.pagingEnabled = YES;
+    self.backgroundColor = [UIColor greenColor];
+    
+    self.layer.cornerRadius = height/2;
+    self.layer.masksToBounds = YES;
+    self.contentSize = CGSizeMake(2*width - height, 0);
+    self.contentOffset = CGPointMake(width - height, 0);
+    
     self.delegate = self;
     
     UIView *leftBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width - height/2, height)];
@@ -64,6 +79,25 @@
     [self addSubview:slider];
 }
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGFloat height = CGRectGetHeight(self.frame);
+    CGFloat width = CGRectGetWidth(self.frame);
+    
+    self.layer.cornerRadius = height/2;
+    self.layer.masksToBounds = YES;
+    
+    _leftBackgroundView.frame = CGRectMake(0, 0, width - height/2, height);
+    _slider.frame = CGRectMake(width - height, 0, height, height);
+    _slider.layer.cornerRadius = height/2;
+    _slider.layer.masksToBounds = YES;
+    
+    if (_titleLabel) {
+        [_titleLabel sizeToFit];
+        _titleLabel.center = CGPointMake(self.bounds.size.width/2 + self.contentOffset.x, self.bounds.size.height/2);
+    }
+}
+
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     CGFloat x = scrollView.contentOffset.x;
     if (0 == x) {
@@ -74,6 +108,71 @@
         if (_sliderHandle) {
             _sliderHandle(NO);
         }
+    }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (_titleLabel) {
+        _titleLabel.center = CGPointMake(self.bounds.size.width/2 + self.contentOffset.x, self.bounds.size.height/2);
+    }
+}
+
+#pragma mark - Action
+- (void)sliderToRight:(void(^)(BOOL isRight))sliderHandle {
+    _sliderHandle = sliderHandle;
+}
+
+#pragma mark - Frame Notifation
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"frame"]) {
+        CGFloat height = CGRectGetHeight(self.frame);
+        CGFloat width = CGRectGetWidth(self.frame);
+        self.contentSize = CGSizeMake(2*width - height, 0);
+        self.contentOffset = CGPointMake(width - height, 0);
+    }
+}
+
+#pragma - Life Cycle
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"frame"];
+}
+
+
+#pragma mark -
+- (void)setTitle:(NSString *)title {
+    _title = title;
+    if (!_titleLabel) {
+        UILabel *label = [[UILabel alloc] init];
+        _titleLabel = label;
+        [self addSubview:label];
+    }
+    _titleLabel.text = title;
+    if (_titleColor) {
+        _titleLabel.textColor = _titleColor;
+    }
+    if (_titleFont) {
+        _titleLabel.font = _titleFont;
+    }
+    
+    [_titleLabel sizeToFit];
+    _titleLabel.center = CGPointMake(self.bounds.size.width/2 + self.contentOffset.x, self.bounds.size.height/2);
+}
+
+- (void)setTitleColor:(UIColor *)titleColor {
+    _titleColor = titleColor;
+    if (_titleLabel) {
+        _titleLabel.textColor = titleColor;
+        [_titleLabel sizeToFit];
+        _titleLabel.center = CGPointMake(self.bounds.size.width/2 + self.contentOffset.x, self.bounds.size.height/2);
+    }
+}
+
+- (void)setTitleFont:(UIFont *)titleFont {
+    _titleFont = titleFont;
+    if (_titleLabel) {
+        _titleLabel.font = titleFont;
+        [_titleLabel sizeToFit];
+        _titleLabel.center = CGPointMake(self.bounds.size.width/2 + self.contentOffset.x, self.bounds.size.height/2);
     }
 }
 
